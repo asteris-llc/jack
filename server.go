f67a23ef641d82a03cbff7b46847d50795d7126e
Brian Hicks <brian@brianthicks.com>
2015-08-18 22:01:51 -0500

    server: deeper inspection of arguments

--- server.go
+++ server.go
@@ -1,12 +1,17 @@
 package jack
 
 import (
+	"bufio"
+	"encoding/json"
 	"errors"
+	"os"
 	"reflect"
 )
 
 var (
 	ErrNoSuchMethod = errors.New("no such method")
+	ErrBadArity     = errors.New("bad arity")
+	ErrBadType      = errors.New("bad type")
 )
 
 type Server struct {
@@ -19,8 +24,24 @@ func NewServer(instance interface{}) *Server {
 	}
 }
 
-func (s *Server) Run() error {
-	return nil
+func (s *Server) Start() error {
+	scanner := bufio.NewScanner(os.Stdin)
+	for scanner.Scan() {
+		req := &Request{}
+		err := json.Unmarshal(scanner.Bytes(), req)
+		if err != nil {
+			return err
+		}
+
+		resp, err := json.Marshal(s.handleCall(req))
+		if err != nil {
+			return err
+		}
+		os.Stdout.Write(resp)
+		os.Stdout.WriteString("\n")
+	}
+
+	return scanner.Err()
 }
 
 func (s *Server) handleCall(message *Request) *Response {
@@ -28,7 +49,7 @@ func (s *Server) handleCall(message *Request) *Response {
 	if !method.IsValid() {
 		return &Response{
 			ID:    message.ID,
-			Error: ErrNoSuchMethod,
+			Error: ErrNoSuchMethod.Error(),
 		}
 	}
 
@@ -37,19 +58,40 @@ func (s *Server) handleCall(message *Request) *Response {
 		values = append(values, reflect.ValueOf(arg))
 	}
 
-	result := method.Call(values)
-	var err error = nil
-	if len(result) > 1 {
-		maybeErr, ok := result[len(result)].Interface().(error)
-		if ok {
-			err = maybeErr
-			result = result[0 : len(result)-1]
+	meta := method.Type()
+	args := []reflect.Value{}
+
+	if len(values) != meta.NumIn() {
+		return &Response{
+			ID:    message.ID,
+			Error: ErrBadArity.Error(),
 		}
+	}
+
+	for i := 0; i < meta.NumIn(); i++ {
+		should := meta.In(i)
+		value := values[i]
+
+		if value.Type().ConvertibleTo(should) {
+			args = append(args, value.Convert(should))
+		} else {
+			return &Response{
+				ID:    message.ID,
+				Error: ErrBadType.Error(),
+			}
+		}
+	}
+
+	result := method.Call(args)
+
+	serializable := []interface{}{}
+	for _, value := range result {
+		serializable = append(serializable, value.Interface())
 	}
 
 	return &Response{
 		ID:      message.ID,
-		Payload: result,
-		Error:   err,
+		Payload: serializable,
+		Error:   "",
 	}
 }
